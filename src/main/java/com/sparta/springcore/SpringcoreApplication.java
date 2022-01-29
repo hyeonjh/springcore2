cf59e246b4b478fd5139b493e878f214f2824167
hyeonjh <97426920+hyeonjh@users.noreply.github.com>
2022-01-29 10:31:48 +0900

    Initial commit

--- src/main/java/com/sparta/springcore/SpringcoreApplication.java
+++ src/main/java/com/sparta/springcore/SpringcoreApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
+@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함 - itemsearchservlet파일
 @SpringBootApplication
 public class SpringcoreApplication {
     public static void main(String[] args) {
